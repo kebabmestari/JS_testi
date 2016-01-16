01ebd022cc7c70c8c090d8a1afa17314f36a04fb
kebabmestari <lindqvist.samuel@gmail.com>
2016-01-17 01:37:15 +0200

    Canvassäätöö

--- testi.js
+++ testi.js
@@ -4,17 +4,35 @@
  * and open the template in the editor.
  */
 
-var closureTesti = ( function(){
-    
-    var luku = 0;
-    
-    return function(){
-        luku++;
-        return luku;
-    };
-    
-}) ();
+var canvason = false;
+var canvas = null;
+
+var ctx = null;
+
+var inte = setInterval( checkCanvas, 2000 );
 
 function funkkari(){
-    document.getElementsByTagName("textarea")[0].value = closureTesti();
+   canvas = document.createElement("canvas");
+   canvas.width = "300";
+   canvas.height = "200";
+   document.body.appendChild(canvas);
+   canvason = true;
+}
+
+function checkCanvas(){
+    if( canvason ){
+        runCanvas();
+        clearInterval( inte );
+    }
+}
+
+function runCanvas(){
+    ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo( 10,10 );
+    ctx.lineTo(200,50);
+    ctx.lineTo(10,100);
+    ctx.closePath();
+    ctx.stroke();
 }
